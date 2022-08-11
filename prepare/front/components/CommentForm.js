f76107bec804ccc1c400246103311c80bc53e62c
jeongmin kang <jm91@bodyfriend.co.kr>
2022-08-11 07:52:38 +0000

    20220811-2

--- prepare/front/components/CommentForm.js
+++ prepare/front/components/CommentForm.js
@@ -2,10 +2,10 @@ import { Form, Input, Button } from "antd";
 import React, { useCallback, useMemo } from "react";
 import PropTypes from "prop-types";
 import useInput from "../hooks/useInput";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 
 const CommentForm = ({ post }) => {
-  const id = useSelector((state) => state.user.me?.id);
+  // const id = useSelector((state) => state.user.me?.id);
   const [commentText, onChangeCommentText] = useInput("");
   const onSubmitComment = useCallback(() => {
     console.log(post.id, commentText);
