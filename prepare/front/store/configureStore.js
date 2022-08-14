6a250858bf21d313d58a3bbea4867de6d9a29a12
sudoku-jm <sudoku0630@gamail.com>
2022-08-14 19:30:55 +0900

    install saga

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,24 +1,30 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore } from "redux";
 import {composeWithDevTools} from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 import reducer from '../reducers'
+import rootSaga from '../sagas';
+
+//3단 고차함수
+const loggerMiddleware = ({dispatch, getState}) => (next) => (action) => {
+  console.log(action);  
+  return next(action);
+}
 
 const configureStore = () => {
   //미들웨어 사용
-  const middlewares = [];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, loggerMiddleware];
   const enhancer = process.env.NODE_ENV === 'production'
   ? compose(applyMiddleware(...middlewares))  //운영(배포)용 : 데브툴 연결x
   : composeWithDevTools(applyMiddleware(...middlewares))  //개발용 : 데브툴 연결 o
 
   const store = createStore(reducer,enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+  return store;
 
   //dispatch 하는 순간 type과 data가 reducer로 전달된다. 그리고 초기state에서 다음 state가 생성된다.
-  store.dispatch({
-    type : 'CHAGNE_NICKNAME',
-    data : 'boogicho',
-  })
 
-  return store;
 };
 const wrapper = createWrapper(configureStore, {
   debug: process.env.NODE_ENV === "development",
