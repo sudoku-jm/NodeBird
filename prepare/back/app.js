30c7ef3894e32895f52519b40511490cc3b1b678
jeongmin kang <jm91@bodyfriend.co.kr>
2022-09-20 08:42:34 +0000

    20220920 backend Node

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,8 +1,30 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  console.log(req.url , req.method);
-  res.end('Hello node');
+const express = require('express');
+const postRouter = require('./routes/post'); 
+
+const app = express(); //호출을 한 번 해야한다.
+
+
+
+app.get('/',(req, res) => {
+  res.send('hello express');
 });
-server.listen(3065, () => {
+
+app.get('/api',(req, res) => {
+  res.send('hello api');
+});
+
+app.get('/posts',(req, res) => {
+  //json 객체로 응답
+  res.json([
+    { id : 1, content: 'hello' },
+    { id : 2, content: 'hello' },
+    { id : 3, content: 'hello' },
+  ]);
+});
+
+//라우터 분리
+app.use('/post',postRouter); 
+
+app.listen(5500, () => {
   console.log('서버 실행 중');
 });
---
+++ prepare/back/routes/post.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/',(req, res) => {
+  // res.json('작성완료');
+  res.json({id : 1, content:'hello'});  //작성된 게시글
+});
+
+router.delete('/',(req, res) => {
+  res.send('hello api');
+});
+
+
+module.exports = router;
